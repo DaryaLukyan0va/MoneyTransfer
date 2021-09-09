4f3bb6fef21e7b7ef2b54882ed4b1c543f3cb859
Daria Lukyanova <luk.dyr@yandex.ru>
2021-09-09 15:02:06 +0300

    MoneyTransfer

---
+++ untitled/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int money = 2000000000;
+        int transfer = 500000000;
+        int all = money + transfer;
+        System.out.println(all);
+
+    }
+}
